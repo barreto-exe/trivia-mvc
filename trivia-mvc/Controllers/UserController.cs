440be9309c57570f651fe4ba96232c602d0f8457
Luis Barreto <luis.barreto.marin@gmail.com>
2021-09-16 23:54:14 -0400

    Lista de usuarios y creando botón agregar user

---
+++ trivia-mvc/Controllers/UserController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using trivia_mvc.DataContexts;
+
+namespace trivia_mvc.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly TriviaContext triviaContext;
+        public UserController(TriviaContext triviaContext)
+        {
+            this.triviaContext = triviaContext;
+        }
+
+
+        // GET: UserController
+        public async Task<IActionResult> Index()
+        {
+            var users = triviaContext.Users.ToList();
+            return View(users);
+        }
+
+        // GET: UserController/Details/5
+        public ActionResult Details(int id)
+        {
+            var user = triviaContext.Users.SingleOrDefault(u => u.IdUser == id);
+
+            return View(user);
+        }
+
+        // GET: UserController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: UserController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: UserController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
